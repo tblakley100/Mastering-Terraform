439f0ca25f66ee488ec8196d6eb87a9c6a3acaa5
Ted Blakley <tblakley@protonmail.com>
2025-09-26 20:08:17 -0500

    feat: Add VPC and subnet resources with configuration validation for networking module

--- tf-main/13-local-modules.TCB/modules/networking/variables.tf
+++ tf-main/13-local-modules.TCB/modules/networking/variables.tf
@@ -1,10 +1,24 @@
-variable "vpc_cidr" {
-  type = string
+variable "vpc_config" {
+  type = object({
+    cidr_block = string
+    name       = string
+  })
 
   validation {
-    condition     = can(cidrnetmask(var.vpc_cidr))
-    error_message = "The vpc_cidr must contain a valid CIDR block."
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
+    error_message = "The cidr_block config option must contain a valid CIDR block."
   }
+
 }
 
-    
+variable "subnet_config" {
+  type = map(object({
+    cidr_block = string
+    az         = string
+  }))
+
+  validation {
+    condition     = alltrue([for subnet in var.subnet_config : can(cidrnetmask(subnet.cidr_block))])
+    error_message = "Each subnet config option must contain a valid CIDR block."
+  }
+}
---
+++ tf-main/13-local-modules.TCB/modules/networking/vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_config.cidr_block
+  tags = {
+    Name = var.vpc_config.name
+  }
+}
+
+resource "aws_subnet" "this" {
+  for_each          = var.subnet_config
+  vpc_id            = aws_vpc.this.id
+  availability_zone = each.value.az
+  cidr_block        = each.value.cidr_block
+
+  tags = {
+    Name = each.key
+  }
+}
